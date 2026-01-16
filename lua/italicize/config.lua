e4ee8f6d11011699b9dc7dc57c66bcf35f43ab63
Aditya Singh Tomar <adityastomar12@gmail.com>
2026-01-16 11:19:24 +0530

    udated list of hl_groups

--- lua/italicize/config.lua
+++ lua/italicize/config.lua
@@ -14,30 +14,75 @@ M.config = {
         "Todo",
     },
     transparent_groups = {
+        -- 1. Base Editor UI
+        "Normal",
+        "NormalNC",
+        "NormalFloat",
+        "FloatBorder",
+        "NonText",
+        "EndOfBuffer",
+        "MsgArea",
+        "Cmdline",
+        "Pmenu",
+
+        -- 2. Sidebar & Dividers
+        "LineNr",
+        "CursorLineNr",
+        "CursorLineNR",
+        "SignColumn",
+        "VertSplit",
+        "WinSeparator",
+
+        -- 3. Syntax Highlighting (From original config)
         "Comment",
         "Conditional",
         "Constant",
-        "CursorLineNr",
-        "CursorLineNR",
-        "EndOfBuffer",
         "Function",
         "Identifier",
-        "LineNr",
-        "NonText",
-        "Normal",
-        "NormalNC",
         "Operator",
         "PreProc",
         "Repeat",
-        "SignColumn",
         "Special",
-        "StalineFilename",
         "Statement",
         "String",
         "Structure",
         "Type",
         "Underlined",
-        "VertSplit",
+
+        -- 4. GitSigns
+        "GitSignsAdd",
+        "GitSignsChange",
+        "GitSignsDelete",
+
+        -- 5. LSP Diagnostic Signs
+        "DiagnosticSignError",
+        "DiagnosticSignWarn",
+        "DiagnosticSignInfo",
+        "DiagnosticSignHint",
+
+        -- 6. LSP Diagnostics (Virtual Text & Underlines)
+        "DiagnosticError",
+        "DiagnosticWarn",
+        "DiagnosticInfo",
+        "DiagnosticHint",
+        "DiagnosticVirtualTextError",
+        "DiagnosticVirtualTextWarn",
+        "DiagnosticVirtualTextInfo",
+        "DiagnosticVirtualTextHint",
+        "DiagnosticUnderlineError",
+        "DiagnosticUnderlineWarn",
+        "DiagnosticUnderlineInfo",
+        "DiagnosticUnderlineHint",
+
+        -- 7. Legacy LSP Groups (Optional support for older plugins)
+        "LspDiagnosticsDefaultError",
+        "LspDiagnosticsDefaultWarning",
+        "LspDiagnosticsDefaultInformation",
+        "LspDiagnosticsDefaultHint",
+
+        -- 8. Custom / Misc
+        "StalineFilename",
+        "SaveAsRoot",
     },
     exclude_transparency_group = {},
     exclude_italics_group = {}
